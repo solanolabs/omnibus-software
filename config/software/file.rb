5490bd06739ecf86618cbfd569402597b94e6c74
Chris Mague <github@mague.com>
2014-05-23 12:05:43 -0700

    add file

---
+++ config/software/file.rb
@@ -0,0 +1,41 @@
+#
+# Copyright:: Copyright (c) 2012-2014 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "file"
+default_version "5.18"
+
+
+version "5.18" do
+  source :md5 => "d420d8f2990cd344673acfbf8d76ff5a"
+end
+
+source :url => "ftp://ftp.astron.com/pub/file/file-#{version}.tar.gz"
+relative_path "file-#{version}"
+
+configure_env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+build do
+  command ["./configure",
+           "--prefix=#{install_dir}/embedded",
+           "--with-libraries=#{install_dir}/embedded/lib"].join(" "), :env => configure_env
+   command "make -j #{max_build_jobs}"
+   command "make -j #{max_build_jobs} install"
+end
